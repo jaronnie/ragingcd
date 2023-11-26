24558136d235a5d9ef64f685f7b300a1ede0e3b4
jaronnie <jaron@jaronnie.com>
2023-11-27 00:29:11 +0800

    feat(fronted): src 别名使用 @

--- fronted/src/main.ts
+++ fronted/src/main.ts
@@ -1,7 +1,7 @@
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 import { createApp } from "vue";
-import App from "./App.vue";
+import App from "@/App.vue";
 
 const app = createApp(App);
 app.use(ElementPlus);
