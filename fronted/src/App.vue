1c02fa103f7daef79b188cc626fcb1cb3ac54bed
jaronnie <jaron@jaronnie.com>
2023-11-27 00:22:54 +0800

    feat(fronted): 添加 element-plus

--- fronted/src/App.vue
+++ fronted/src/App.vue
@@ -1,9 +1,18 @@
 <template>
   <div>
-    <h1>我是 App</h1>
+    <el-button type="primary" :icon="Plus">Primary</el-button>
+
+    <el-pagination
+      :page-size="20"
+      :pager-count="11"
+      layout="prev, pager, next"
+      :total="1000"
+    />
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Plus } from "@element-plus/icons-vue";
+</script>
 
 <style scoped></style>
