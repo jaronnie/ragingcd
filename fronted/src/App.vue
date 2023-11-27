47f23d2ccb9ccc0e0842fe28ece37a0133df1be8
jaronnie <jaron@jaronnie.com>
2023-11-28 02:56:01 +0800

    feat(fronted): 登录页静态页面

--- fronted/src/App.vue
+++ fronted/src/App.vue
@@ -1,42 +1,7 @@
 <template>
-  <div>
-    <el-button type="primary" :icon="Plus">Primary</el-button>
-
-    <h1>我是根 App</h1>
-
-    <el-pagination
-      :page-size="20"
-      :pager-count="11"
-      layout="prev, pager, next"
-      :total="1000"
-    />
-
-    <SvgIcon name="setting" width="50px" height="50px"></SvgIcon>
-
-    <router-view></router-view>
-  </div>
+  <router-view></router-view>
 </template>
 
-<script setup lang="ts">
-import { Plus } from "@element-plus/icons-vue";
+<script setup lang="ts"></script>
 
-// 测试 mock 接口
-import { onMounted } from "vue";
-import { reqLogin, reqUserInfo } from "@/api/user";
-
-onMounted(() => {
-  reqLogin({
-    username: "admin",
-    password: "123456",
-  });
-  reqUserInfo();
-});
-</script>
-
-<style scoped lang="scss">
-div {
-  h1 {
-    color: $color;
-  }
-}
-</style>
+<style scoped lang="scss"></style>
--- fronted/src/views/login/index.vue
+++ fronted/src/views/login/index.vue
@@ -1,9 +1,74 @@
 <template>
-  <div>
-    <h1>我是 login</h1>
+  <div class="login_container">
+    <el-row>
+      <el-col :span="12" :xs="0"></el-col>
+      <el-col :span="12" :xs="24">
+        <el-form class="login_form">
+          <h1>你好世界!</h1>
+          <h2>by jaronnie</h2>
+          <el-form-item>
+            <el-input
+              :prefix-icon="User"
+              v-model="loginForm.username"
+            ></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-input
+              type="password"
+              :prefix-icon="Lock"
+              showPassword
+              v-model="loginForm.password"
+            ></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button class="login_btn" type="primary" size="default"
+              >登录</el-button
+            >
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { User, Lock } from "@element-plus/icons-vue";
+import { reactive } from "vue";
 
-<style scoped></style>
+let loginForm = reactive({
+  username: "admin",
+  password: "123456",
+});
+</script>
+
+<style scoped lang="scss">
+.login_container {
+  width: 100%;
+  height: 100vh;
+  background: url("@/assets/images/background.jpg") no-repeat;
+  background-size: cover;
+}
+.login_form {
+  position: relative;
+  width: 80%;
+  top: 30vh;
+  background: url("@/assets/images/login_form.png") no-repeat;
+  background-size: cover;
+  padding: 40px;
+}
+
+h1 {
+  color: white;
+  font-size: 40px;
+}
+
+h2 {
+  font-size: 20px;
+  color: white;
+  margin: 20px 0px;
+}
+
+.login_btn {
+  width: 100%;
+}
+</style>
