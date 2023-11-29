ef9ca08caf91b607056718558d1bd6aa746d4393
jaronnie <jaron@jaronnie.com>
2023-11-30 00:33:05 +0800

    feat(fronted): 设置没有 token 或则 token 失效时自动跳转到 login

--- fronted/src/views/home/index.vue
+++ fronted/src/views/home/index.vue
@@ -6,6 +6,21 @@
 
 <script setup lang="ts">
 import useUserStore from "@/store/modules/user";
+import { onMounted } from "vue";
+import { useRouter } from "vue-router";
+
+// 获取路由器
+let $router = useRouter();
+
+onMounted(async () => {
+  try {
+    await userStore.userInfo();
+    $router.push("/");
+  } catch (error) {
+    $router.push("/login");
+  }
+});
+
 let userStore = useUserStore();
 userStore.userInfo();
 </script>
